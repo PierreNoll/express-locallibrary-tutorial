0b7f2946eb8f3fe5d6a913938dce797ad394f566
NOLL Pierre <Pierre.NOLL@egis.fr>
2018-10-21 16:12:36 +0200

    all to book detail page

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -17,6 +18,13 @@ BookInstanceSchema
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
 });
+
+// Virtual for formatting the due_date
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function(){
+  return moment(this.due_back).format('MMMM Do, YYYY');
+})
 
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
