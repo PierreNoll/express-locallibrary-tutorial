627fe3f031dd6d4c32bd434cca4847220fb8fc4b
NOLL Pierre <Pierre.NOLL@egis.fr>
2018-10-27 23:46:29 +0200

    debug author create_author_post

--- models/author.js
+++ models/author.js
@@ -47,5 +47,19 @@ AuthorSchema
   return this.date_of_death ? moment(this.date_of_death).format('YYYY/MM/DD') : '';
 });
 
+// Virtual for date of birth formatting for form
+AuthorSchema
+.virtual('date_of_birth_formatted_form')
+.get(function () {
+  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+// Virtual for date of death formatting for form
+AuthorSchema
+.virtual('date_of_death_formatted_form')
+.get(function () {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
